229d44c925878e982e80b19f32227033fc418208
영철 이 <snnmhks@naver.com>
2022-11-04 15:25:25 +0900

    초기 커밋.

---
+++ 소스.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string word, vector<string> pages) 
+{
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    string a = "a";
+    int b = static_cast<int>(a[0]);
+    b -= 32;
+    a[0] = static_cast<char>(b);
+    cout << a;
+    int j;
+    cin >> j;
+}
