531695bfc16a04ceb3681859ecbd3741dc16185a
영철 이 <snnmhks@naver.com>
2022-11-04 15:30:10 +0900

    aaa

--- 소스.cpp
+++ 소스.cpp
@@ -4,6 +4,28 @@
 
 using namespace std;
 
+int CompareWord(char a,char b)
+{
+    int a_num = static_cast<int>(a);
+    int b_num = static_cast<int>(b);
+    if (a_num >= 97 && b_num <= 90)
+    {
+        a_num -= 32;
+    }
+    else if (a_num <= 90 && b_num >= 97)
+    {
+        b_num -= 32;
+    }
+    if (a_num == b_num)
+    {
+        return 1;
+    }
+    else if (a_num != b_num)
+    {
+        return 0;
+    }
+}
+
 int solution(string word, vector<string> pages) 
 {
     int answer = 0;
